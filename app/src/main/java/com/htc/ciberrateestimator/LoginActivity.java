5ddad7b6551225059950054ed9e88f18ce64bc70
mohann <nmohan1995@gmail.com>
2020-03-02 16:26:56 +0530

    added login screen

---
+++ app/src/main/java/com/htc/ciberrateestimator/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.htc.ciberrateestimator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btLogin;
+    private EditText etUsername, etPassword;
+    private Map<String,String> map;
+    private LinearLayout llLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btLogin = findViewById(R.id.btLogin);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        llLogin = findViewById(R.id.llLogin);
+
+        map = new HashMap<>();
+
+        map.put("Kishore","123@Welcome");
+        map.put("Ramki","123Welcome@");
+        map.put("Vel","123Welcome");
+        map.put("Mohan","12345@Welcome");
+        map.put("Selvakumar","12345Welcome@");
+        map.put("HTCUSER","12345Welcome");
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(validateAllFields()){
+                    String username = etUsername.getText().toString().trim();
+                    String password = etPassword.getText().toString().trim();
+
+                    if(password.equals(map.get(username))) {
+                        Intent intent = new Intent(LoginActivity.this, NumberOfResourcesActivity.class) ;
+                        intent.putExtra(getString(R.string.USERNAME),""+username);
+                        startActivity(intent);
+                    } else {
+//                        Toast.makeText(LoginActivity.this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
+                        Snackbar snackbar = Snackbar.make(llLogin, "Username or Password is incorrect", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    private boolean validateAllFields() {
+        boolean status = true;
+        if (etUsername.getText().toString().isEmpty()) {
+            etUsername.setError("Username is required");
+            status = false;
+        }
+        if (etPassword.getText().toString().isEmpty()) {
+            etPassword.setError("Password is required");
+            status = false;
+        }
+        return status;
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+}
