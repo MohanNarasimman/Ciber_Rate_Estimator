0cf3220fcaab23381f5d0206c966c08af28076e9
mohann <nmohan1995@gmail.com>
2020-03-03 11:06:53 +0530

    added keyboard functionality for login screen

--- app/src/main/java/com/htc/ciberrateestimator/LoginActivity.java
+++ app/src/main/java/com/htc/ciberrateestimator/LoginActivity.java
@@ -2,9 +2,12 @@ package com.htc.ciberrateestimator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -15,11 +18,11 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     private Button btLogin;
     private EditText etUsername, etPassword;
-    private Map<String,String> map;
+    private Map<String, String> map;
     private LinearLayout llLogin;
 
     @Override
@@ -33,24 +36,24 @@ public class LoginActivity extends AppCompatActivity {
 
         map = new HashMap<>();
 
-        map.put("Kishore","123@Welcome");
-        map.put("Ramki","123Welcome@");
-        map.put("Vel","123Welcome");
-        map.put("Mohan","12345@Welcome");
-        map.put("Selvakumar","12345Welcome@");
-        map.put("HTCUSER","12345Welcome");
+        map.put("Kishore", "123@Welcome");
+        map.put("Ramki", "123Welcome@");
+        map.put("Vel", "123Welcome");
+        map.put("Mohan", "12345@Welcome");
+        map.put("Selvakumar", "12345Welcome@");
+        map.put("HTCUSER", "12345Welcome");
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(validateAllFields()){
+                if (validateAllFields()) {
                     String username = etUsername.getText().toString().trim();
                     String password = etPassword.getText().toString().trim();
 
-                    if(password.equals(map.get(username))) {
-                        Intent intent = new Intent(LoginActivity.this, NumberOfResourcesActivity.class) ;
-                        intent.putExtra(getString(R.string.USERNAME),""+username);
+                    if (password.equals(map.get(username))) {
+                        Intent intent = new Intent(LoginActivity.this, NumberOfResourcesActivity.class);
+                        intent.putExtra(getString(R.string.USERNAME), "" + username);
                         startActivity(intent);
                     } else {
 //                        Toast.makeText(LoginActivity.this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
@@ -80,5 +83,15 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            assert imm != null;
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
